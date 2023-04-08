066a6d35d30807174eb066637947acb7de1f4441
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-04-08 17:38:37 +0100

    feat: add fabfile to clean up `hbnb` release archives

---
+++ 100-clean_web_static.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Fabric script to delete out-of-date archives"""
+
+from fabric.api import env, local, lcd, cd, sudo
+
+env.hosts = ['18.234.192.255', '54.164.58.89']
+env.user = 'ubuntu'
+
+
+def do_clean(number=0):
+    """Deletes out-of-date archives"""
+
+    # Convert number to int
+    try:
+        number = int(number)
+    except ValueError:
+        return
+
+    # Check that number is not negative
+    if number < 0:
+        return
+    elif number == 0:
+        number = 1
+
+    # Keep the most recent number of archives
+    number += 1
+
+    # Delete old archives in versions folder
+    with lcd("versions"):
+        local("ls -t | tail -n +{} | xargs rm -rf".format(number))
+
+    # Delete old archives in web servers
+    with cd("/data/web_static/releases"):
+        sudo("ls -t | tail -n +{} | xargs rm -rf".format(number))
+
+    # Delete symbolic links that are broken
+    with cd("/data/web_static/current/"):
+        sudo("ls -l | awk '{print $9}' | grep -v ^$ | "
+             "grep -v ^web_static | xargs rm -rf")
