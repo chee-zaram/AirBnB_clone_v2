787a6f491f79f1529ef8d815cd318a7c82a80548
Emmanuel Chee-zaram Okeke <ecokeke21@gmail.com>
2023-04-24 15:18:41 +0100

    fix(9-states.html): display `States` title even when no state in database

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -14,7 +14,7 @@ def states(id=""):
 
     states = storage.all(State).values()
 
-    if states and not id:
+    if not id:
         return render_template('9-states.html', states=states, lone_state=None)
 
     for st in states:
